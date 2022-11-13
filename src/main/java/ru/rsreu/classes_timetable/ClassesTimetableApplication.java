5397d2046bbf4299fa2ecb131c6e3bed02f6485b
Konstantin Polshchikov <konstantin_polshchikov@epam.com>
2022-11-13 15:00:23 +0300

    initial commit

---
+++ src/main/java/ru/rsreu/classes_timetable/ClassesTimetableApplication.java
@@ -0,0 +1,13 @@
+package ru.rsreu.classes_timetable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassesTimetableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassesTimetableApplication.class, args);
+	}
+
+}
